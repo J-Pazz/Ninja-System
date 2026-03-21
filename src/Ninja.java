b238358b30a7cf91e7d87e62514ffb03775b6394
joaob <joaolucp@gmail.com>
2026-03-21 11:52:53 -0300

    adicionando classe Ninja

--- src/Ninja.java
+++ src/Ninja.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Ninja {
+    String nome;
+    int idade;
+    Missao missao;
+    //ai aiii
+    public Ninja( String nome, int idade, Missao missao){
+        this.nome = nome;
+        this.idade = idade;
+        this.missao = missao;
+        //condição
+        if (idade < 16){
+            System.out.println("Ele não pode ser ninja por causa da idade!");
+        } else{
+            System.out.println("Ele tem idade.");
+        }
+//a
+    }
+
 }
