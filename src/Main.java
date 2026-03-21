bc95f1d0c484825b9b4433ed2ed2db8c2afe7e5c
joaob <joaolucp@gmail.com>
2026-03-21 11:50:16 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Missao m1 = new Missao("Capturar um gato.", "Médio", true);
+        Ninja n1 = new Ninja("Naruto", 16, m1);
+//a
+        System.out.println("Missao " + n1.missao.nomee);
+        System.out.println("Naruto pode ser um ninja!");
     }
+
+}
 }
